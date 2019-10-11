5da4e06365c69eac435bf18880b3e7cf3a7c765d
Madhavi Thakur <madhavidadhwal91@gmail.com>
2019-10-11 04:54:03 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+class School
+  attr_accessor :roster
+
+  def initialize(name)
+  @roster = {} 
+  @name = name
+  end 
+ 
+  def add_student(name, grade)
+    if @roster.include?(grade)
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
+    end
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort
+    result = {}
+    Hash[roster.sort_by{|k,v| k}].map do |k,v|
+      result[k] = v.sort
+    end
+    result
+  end 
+  
+end 
